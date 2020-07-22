46af0150c6a51096b7a29959ef201151d551560f
MINSOO KANG <stengine2@gmail.com>
2020-07-22 17:17:47 +0900

    Before moving the project to WPF

--- smplParser.py
+++ smplParser.py
@@ -1,29 +1,31 @@
 import json
-f1=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master.txt",'r',encoding='UTF-8')
-f2=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master2.smpl",'r',encoding='UTF-8')
-f3=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master3.smpl",'r',encoding='UTF-8')
-f4=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master4.smpl",'r',encoding='UTF-8')
+import tkinter as tk
+from tkinter import filedialog
+import sys
+import os
 
-def smplParser(smpl):
-    smpl=smpl[13:]
-    temp=smpl.index("]",len(smpl)*98//100)
-    smpl=smpl[:temp-1]
-    musicList=smpl.split("},{")
-    musicList2=[]
-    for i in musicList[:10]:
-        i=i[1:]
-        musicList2.append(i.split('","'))
-    for i in musicList2:
-        print(i)
+def loadFiles(verbose=False):
+    files=[]
+    if verbose:
+        filePaths = fd.askopenfilenames()
+        for i in filePaths:
+            files.append(open(i,'r',encoding='UTF-8'))
+    else:
+        f1=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master.txt",'r',encoding='UTF-8')
+        f2=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master2.smpl",'r',encoding='UTF-8')
+        f3=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master3.smpl",'r',encoding='UTF-8')
+        f4=open("D:/Dropbox/Workspace/03 Python/03 Smpl-Editor/Master4.smpl",'r',encoding='UTF-8')
+        files=[f1,f2,f3,f4]
+    return files
 
-
-#smplParser(f1.readline())
-x=json.loads(f1.readline())["members"]
+def display(lines):
+    os.system('cls')
+    for line in lines:
+        print(line)
+    
+x=json.loads(f1.readline())["members"][:1]
 for i in range(len(x)):
     try:
         print(str(i)+" "+str(x[i]))
     except:
         print(i)
-
-
-
