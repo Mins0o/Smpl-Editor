918598e1b7f1ffa3847f1ec16f7875c9e3cd2d5e
MINSOO KANG <stengine2@gmail.com>
2020-07-22 23:49:43 +0900

    Loading in the smpl as array

--- SmplEditor/MainWindow.xaml.cs
+++ SmplEditor/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using System.Text.Json;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,6 +26,29 @@ namespace SmplEditor
         public MainWindow()
         {
             InitializeComponent();
+            var a = new SmplLoader();
+            a.LoadSmpl();
+        }
+    }
+
+    public class SmplLoader
+    {
+        public Song[] LoadSmpl()
+        {
+            OpenFileDialog openSmpls = new OpenFileDialog();
+            openSmpls.Multiselect = true;
+            openSmpls.ShowDialog();
+            Song[] result = new Song[0];
+            foreach (string openedSmpl in openSmpls.FileNames)
+            {
+                Song[] readInList = JsonSerializer.Deserialize<Smpl>(File.ReadAllText(openedSmpl)).members;
+                Song[] temp = new Song[result.Length];
+                result.CopyTo(temp,0);
+                result = new Song[temp.Length + readInList.Length];
+                temp.CopyTo(result, 0);
+                readInList.CopyTo(result, temp.Length);
+            }
+            return result;
         }
     }
 }
---
+++ SmplEditor/Smpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmplEditor
+{
+    public class Smpl
+    {
+        public Song[] members { get; set; }
+    }
+    public class Song
+    {
+        public string artist { get; set; }
+        public string info { get; set; }
+        public int order { get; set; }
+        public string title { get; set; }
+        public int type { get; set; }
+    }
+}
