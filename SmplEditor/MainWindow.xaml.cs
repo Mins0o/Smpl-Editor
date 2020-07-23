24f0008ca94ea39ce44017c3d4bca376c673cee8
MINSOO KANG <stengine2@gmail.com>
2020-07-23 17:20:50 +0900

    Import Smpl and display them

--- SmplEditor/MainWindow.xaml.cs
+++ SmplEditor/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
 using System.Text.Json;
+using Microsoft.Win32;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,32 +23,76 @@ namespace SmplEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string,Smpl> Playlists;
         public MainWindow()
         {
+            while (Playlists == null){
+                try
+                {
+                    Playlists = this.LoadSmpls();
+                }
+                catch (ArgumentException e)
+                {
+                    if (e.Data.Contains("shortName"))
+                    {
+                        string shortName=(string)e.Data["shortName"];
+                        MessageBox.Show(shortName + ": " + e.Message);
+                    }
+                }
+            }
             InitializeComponent();
-            var a = new SmplLoader();
-            a.LoadSmpl();
+            PlaylistsBox.ItemsSource = Playlists.Keys;
+            SongsListBox.ItemsSource = AllSongs();
         }
-    }
 
-    public class SmplLoader
-    {
-        public Song[] LoadSmpl()
+        public Dictionary<string,Smpl> LoadSmpls()
         {
             OpenFileDialog openSmpls = new OpenFileDialog();
             openSmpls.Multiselect = true;
             openSmpls.ShowDialog();
-            Song[] result = new Song[0];
-            foreach (string openedSmpl in openSmpls.FileNames)
+            string[] shortNames=openSmpls.SafeFileNames;
+
+            Dictionary<string,Smpl> playlists = new Dictionary<string,Smpl>();
+            for(int i=0;i<shortNames.Length;i++)
             {
-                Song[] readInList = JsonSerializer.Deserialize<Smpl>(File.ReadAllText(openedSmpl)).members;
-                Song[] temp = new Song[result.Length];
-                result.CopyTo(temp,0);
-                result = new Song[temp.Length + readInList.Length];
-                temp.CopyTo(result, 0);
-                readInList.CopyTo(result, temp.Length);
+                string shortName = shortNames[i];
+                string jsonString = File.ReadAllText(openSmpls.FileNames[i]);
+                Smpl playlist = JsonSerializer.Deserialize<Smpl>(jsonString);
+                int extensionStartIndex = shortName.IndexOf('.',shortName.Length-5);
+                shortName = shortName.Substring(0,extensionStartIndex);
+                playlist.name = shortName;
+                try
+                {
+                    playlists.Add(shortName, playlist);
+                }
+                catch (ArgumentException e)
+                {
+                    e.Data.Add("shortName", shortName);
+                    throw;
+                }
+
+
             }
-            return result;
+            return playlists;
+        }
+
+        public Song[] AllSongs()
+        {
+            List<Song> allSongs = new List<Song>();
+            foreach (Smpl playlist in Playlists.Values)
+            {
+                allSongs.AddRange(playlist.members);
+            }
+            return allSongs.ToArray();
+        }
+
+        public void CreatePlaylist(string name)
+        {
+            Playlists.Add(name, new Smpl());
+        }
+        public void DeletePlaylist(string name)
+        {
+            Playlists.Remove(name);
         }
     }
 }
--- SmplEditor/Smpl.cs
+++ SmplEditor/Smpl.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using Microsoft.Win32;
 
 namespace SmplEditor
 {
     public class Smpl
     {
-        public Song[] members { get; set; }
+        public string name;
+        public List<Song> members { get; set; }
+        public void AddSong(Song addition)
+        {
+            this.members.Add(addition);
+        }
+        public void AddSongs(List<Song> additions)
+        {
+            this.members.AddRange(additions);
+        }
+        public void AddSongs(Song[] additions)
+        {
+            this.members.AddRange(additions);
+        }
+        public override string ToString()
+        {
+            return this.name;
+        }
     }
     public class Song
     {
@@ -17,5 +37,9 @@ namespace SmplEditor
         public int order { get; set; }
         public string title { get; set; }
         public int type { get; set; }
+        public override string ToString()
+        {
+            return artist +"-" +title;
+        }
     }
 }
